1ea806fe48d36e19749d0c3f2cf61dabca29d9da
Conteusz81 <piotr.pawlica@interia.pl>
2020-10-08 22:00:40 +0200

    NewsFeed pagination added

--- src/components/organisms/NewsFeed.js
+++ src/components/organisms/NewsFeed.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Header from "../molecules/Header";
 import NewsCard from "../molecules/NewsCard";
 
@@ -7,7 +8,7 @@ const NewsFeed = ({ pathname }) => {
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        axios.get(`https://content.guardianapis.com/${pathname}?api-key=8be27dec-dac1-4a6c-8749-0e519052a797`)
+        axios.get(`https://content.guardianapis.com/${pathname}?page=1&page-size=10&api-key=8be27dec-dac1-4a6c-8749-0e519052a797`)
             .then(res => {
                 const news = res.data.response.results;
                 setNews(news);
@@ -19,7 +20,22 @@ const NewsFeed = ({ pathname }) => {
         <div>
             <Header title={pathname} />
             <div>
-                <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Last news</h4>
+
+                <div className="flex items-end mt-12 pb-2 border-b border-gray-200 font-bold">
+                    <h4 className="flex-grow ">Last news</h4>
+                    <div className="flex text-xl items-center">
+                        <div className="text-2xl cursor-pointer">
+                            <FaChevronLeft />
+                        </div>
+                        <div className="mx-3 pt-1">
+                           Page 10
+                        </div>
+                        <div className="text-2xl cursor-pointer">
+                            <FaChevronRight />
+                        </div>
+                    </div>
+
+                </div>
                 <div className="mt-8 grid gap-10">
                     {
                         news && news.map(article => <NewsCard
