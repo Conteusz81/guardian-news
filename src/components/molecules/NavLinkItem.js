6f2090a0cfe69e8cd4dc1a7d9eac336f4a9c4b4a
Conteusz81 <piotr.pawlica@interia.pl>
2020-10-08 15:54:03 +0200

    NavLinkItem refactor

--- src/components/molecules/NavLinkItem.js
+++ src/components/molecules/NavLinkItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import { routes } from "../../routes/routes";
 import { pathNameSlice } from "../../utils/parsers";
 
 const NavLinkItem = ({ route }) => {
-    const navLinkName = route === routes.world ? '/world' : route;
 
     return (
         <li className="py-1">
-            <NavLink to={route} className="px-4 flex justify-end border-r-4 border-white" activeClassName="font-bold border-primary">
-                <span className="capitalize">{pathNameSlice(navLinkName)}</span>
+            <NavLink
+                to={route}
+                className="px-4 flex justify-end border-r-4 border-white"
+                activeClassName="font-bold border-primary"
+            >
+                <span className="capitalize">{pathNameSlice(route)}</span>
             </NavLink>
         </li>
     );
