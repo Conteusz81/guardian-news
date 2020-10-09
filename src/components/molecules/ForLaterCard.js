4ee85768b51bdfdce6d4df1c3a5d0e4bbe163b31
Conteusz81 <piotr.pawlica@interia.pl>
2020-10-09 08:51:32 +0200

    forLaterCard remove function

--- src/components/molecules/ForLaterCard.js
+++ src/components/molecules/ForLaterCard.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { IoMdRemoveCircleOutline } from "react-icons/io";
+import { useForLaterList } from "../../store/context/ForLaterContext";
 
 const ForLaterCard = ({ id, title, externalUrl, section }) => {
+    const { dispatch } = useForLaterList();
+
+    const removeFromForLaterList = () => {
+        dispatch({ type: "REMOVE_ARTICLE", id});
+    }
+
     return (
-        <div className="w-full mb-2 flex border border-gray-400 bg-gray-100 rounded">
-            <div className="p-2 hover:bg-white">
+        <div className="w-full mb-2  flex border border-gray-400 bg-gray-100 rounded ">
+            <div className="flex flex-col flex-grow py-1 px-2 hover:bg-white">
                 <a href={externalUrl} rel="noopener noreferrer" target="_blank">
-                    <div className="text-gray-600 text-sm">{title}</div>
+                    <h5 className="capitalize text-xs">{section}</h5>
+                    <div className="mt-2 text-gray-600 text-sm">{title}</div>
                 </a>
+            </div>
+            <div
+                className="flex items-center justify-center p-2 border-l border-gray-400 cursor-pointer hover:bg-white"
+                onClick={removeFromForLaterList}
+            >
+                <IoMdRemoveCircleOutline />
             </div>
         </div>
     );
--- src/store/reducers/ForLaterReducer.js
+++ src/store/reducers/ForLaterReducer.js
@@ -6,10 +6,13 @@ export const forLaterListReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_ARTICLE": {
             if(articleExists(state, action.payload)) {
-                return state
+                return state;
             } else {
-                return [...state, action.payload]
+                return [...state, action.payload];
             }
+        }
+        case "REMOVE_ARTICLE": {
+            return state.filter(article => article.id !== action.id);
         }
         default: return state;
     }
